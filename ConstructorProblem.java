783d2ecebbd4d63e0478d961e93d0b0ace841df7
rayhanrashid <12345678asdf>
2024-11-01 21:06:48 -0400

    working COnstructors

---
+++ ConstructorProblem.java
@@ -0,0 +1,27 @@
+class A{
+    A(){
+        System.out.println("Default Constructor");
+    }
+    A(int val){
+        System.out.println("parameterised " + val);
+    }
+}
+
+class B extends A{
+    B(){
+        super();
+        // super(5); // THis will give compiler error as always super() or any version always first
+        System.out.println("B Constructor");
+        // super(5); // THis will give compiler error as always super() or any version always first
+    }
+}
+
+/**
+ * ConstructorProblem
+ */
+public class ConstructorProblem {
+
+    public static void main(String... args) {
+        A b = new B();
+    }
+}
